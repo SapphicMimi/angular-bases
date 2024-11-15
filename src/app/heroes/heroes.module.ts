c71688795d0eb7bd528c0be04d6ec648d28a2ef2
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-15 16:42:48 +0100

    Fin de la Sección 4

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from "@angular/common";
 import { HeroComponent } from "./hero/hero.component";
 import { ListComponent } from "./list/list.component";
 
-
 @NgModule({
   declarations: [
     HeroComponent,
