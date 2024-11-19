67aafe0b5c020ecb6b43dd21dfcdbf286f3c546b
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-19 16:51:21 +0100

    Hasta Servicios

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  DeleteCharacter(index: number): void {
+    this.onDelete.emit(index);
+  }
+}
